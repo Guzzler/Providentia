831c7d15f53f7e2c47d57a1ce17b2fdc7194d2fc
Guzzler <sharangpai123@gmail.com>
2018-11-01 21:29:18 +0000

    incorporating all 4 components

--- app/views.py
+++ app/views.py
@@ -6,12 +6,27 @@ import os,os.path
 def index():
     predictedimages = os.listdir('./app/static/img/predicted')
     predictedimages = ['img/predicted/'+file for file in predictedimages]
-    return render_template('index.html',images=predictedimages)
+    humid = open('./app/static/env/humid.txt', 'r') 
+    temp = open('./app/static/env/temp.txt', 'r') 
+    dist = open('./app/static/env/dist.txt', 'r')  
+    return render_template('index.html',images=predictedimages,distance=dist.read(),humidity=humid.read(),temperature=temp.read())
 
 @app.route('/upload',methods=['POST'])
 def upload():
     print(request.files['media'])
     file = request.files['media']
+    distance = request.files['Distance']
+    humidity= request.files['Humidity']
+    temperature = request.files['Temperature']
+    humid = open('./app/static/env/humid.txt', 'w')
+    temp = open('./app/static/env/temp.txt', 'w')
+    dist = open('./app/static/env/dist.txt', 'w')
+    humid.write(str(humidity))
+    humid.close()
+    temp.write(str(temperature))
+    temp.close()
+    dist.write(str(distance))
+    dist.close()
     newfilename= len([name for name in os.listdir('./app/static/img/unpredicted/') if os.path.isfile(os.path.join('./app/static/img/unpredicted/',name))])
     print(newfilename)
     newfilename= str(newfilename)
@@ -19,4 +34,4 @@ def upload():
     os.system('python app/prediction.py app/static/img/unpredicted/'+newfilename+'.jpg'+' app/static/img/predicted/'+newfilename+'.jpg'+' app/static/thumbs/img/predicted/'+newfilename+'.jpg')
     predictedimages = os.listdir('./app/static/img/predicted')
     predictedimages = ['/img/predicted/'+file for file in predictedimages]
-    return render_template('index.html',images=predictedimages)
+    return render_template('index.html',images=predictedimages,distance=str(distance),humidity=str(humidity),temperature=str(temperature))
